6a50c570d18e0f55c8f9ea06be84a93db41e5fd4
Vikram Singh Bedi <vikram.bedi97@gmail.com>
2024-07-06 16:51:33 -0400

    added models

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from store.models import Product, Category, Order, Customer
+
 # Register your models here.
+admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(Order)
+admin.site.register(Customer)
