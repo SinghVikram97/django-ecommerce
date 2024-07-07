ebbcd67c2bc528dc33a461658360b7f07def43dd
anirudh-grack <anirudhgrack@gmail.com>
2024-07-07 14:27:27 -0400

    Shopping cart setup: New app cart Created along with Customer Sessions and Add Product To Cart

---
+++ cart/cart.py
@@ -0,0 +1,17 @@
+class Cart():
+    def __init__(self, request):
+        self.session = request.session
+        # If current session_key exists, get it!
+        cart = self.session.get('session_key')
+        # If user new no session_key ! Create one
+        if 'session_key' not in request.session:
+            cart = self.session['session_key'] = {}
+        # To make shopping cart work on all pages of website
+        self.cart = cart
+    def add(self, product):
+        product_id = str(product.id)
+        if product_id in self.cart:
+            pass
+        else:
+            self.cart[product_id] = {'price': str(product.price)}
+        self.session.modified = True
