54fd244a3fab222e36e552a11d45438f3672beab
anirudh-grack <anirudhgrack@gmail.com>
2024-07-08 03:48:05 -0400

    Addded Option to delete items from shopping cart

--- cart/cart.py
+++ cart/cart.py
@@ -47,3 +47,10 @@ class Cart():
 
         thing = self.cart
         return thing
+    def delete(self, product):
+        product_id = str(product)
+        # Delete from dictionary
+        if product_id in self.cart:
+            del self.cart[product_id]
+        self.session.modified = True
+
