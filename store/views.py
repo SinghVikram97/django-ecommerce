942f52c2fcac20173a0061c899df7e0a3ab475b1
sakshamthukral <thukral.saksham132@gmail.com>
2024-07-12 16:00:55 -0400

    update-user-password

--- store/views.py
+++ store/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .forms import SignUpForm, UpdateUserForm
+from .forms import SignUpForm, UpdateUserForm, ChangePasswordForm
 from django import forms
 
 
@@ -31,7 +31,29 @@ def category(request, categoryselected):
         messages.success(request, "Category does not exist")
         return redirect('home')
 
+def update_password(request):
+    if request.user.is_authenticated:
+        current_user = request.user
+        if request.method == 'POST':
+            form = ChangePasswordForm(user=current_user, data=request.POST)
 
+            # is the form valid
+            if form.is_valid():
+                form.save()
+                messages.success(request, 'Password has been updated')
+                login(request, current_user)
+                return redirect('home')
+            else:
+                for error in list(form.errors.values()):
+                    messages.error(request, error)
+                    return redirect('update_password')
+                   
+        else:
+            form = ChangePasswordForm(user=current_user)
+            return render(request, 'update_password.html', {'form': form})
+    else:
+        messages.success(request, 'Please login to update your password')
+        return redirect('home')
 def update_user(request):
     if request.user.is_authenticated:
         current_user = User.objects.get(id=request.user.id)
