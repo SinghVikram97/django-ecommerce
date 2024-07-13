29c40cbec37c7241ec2de374eda0f13c7268fbac
Arnab Sinha <sarnab433@gmail.com>
2024-07-13 10:32:04 -0400

    Added forgot password

--- store/urls.py
+++ store/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('login/', views.login_user, name='login'),
+    path('accounts/login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
     path('register/', views.register_user, name='register'),
     path('update_password/', views.update_password, name='update_password'),
@@ -13,5 +15,8 @@ urlpatterns = [
     path('product/<int:pk>', views.product, name='product'),
     path('category/<str:categoryselected>', views.category, name='category'),
     path('search/', views.search, name='search'),
-
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
